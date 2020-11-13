0f09a664d695cbbcbdf275724bdf977ad6bd047a
hjl <jingliqaq@gmail.com>
2020-11-13 23:39:40 +0800

    访问test接口

---
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.soft1851.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public Object hello(){
+        return "hello";
+    }
+}
