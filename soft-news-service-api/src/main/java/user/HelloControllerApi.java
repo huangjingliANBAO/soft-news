87bc39d873ef7c6639f8ee1752ce9062b2268fad
hjl <jingliqaq@gmail.com>
2020-11-14 21:54:48 +0800

    配置logback日志与多环境profile

---
+++ soft-news-service-api/src/main/java/user/HelloControllerApi.java
@@ -0,0 +1,19 @@
+package user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * HelloControllerApi接口
+ * @author ysx
+ */
+
+public interface HelloControllerApi {
+
+    /**
+     * hello接口
+     *
+     * @return Object
+     */
+    @GetMapping("/hello")
+    Object hello();
+}
--- soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
+++ soft-news-service-user/src/main/java/com/soft1851/user/controller/HelloController.java
@@ -1,12 +1,18 @@
 package com.soft1851.user.controller;
 
+import com.soft1851.result.GraceResult;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import user.HelloControllerApi;
 
 @RestController
-public class HelloController {
-    @GetMapping("/hello")
+@Slf4j
+public class HelloController implements HelloControllerApi {
+    @Override
     public Object hello(){
-        return "hello";
+        log.info("a");
+        log.debug("b");
+        return GraceResult.ok("hello");
     }
 }
