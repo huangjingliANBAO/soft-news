e31fb4df32d8af385f85b55ebc4ea9887cd5a77e
hjl <jingliqaq@gmail.com>
2020-11-15 19:39:37 +0800

    整合mybatis

---
+++ soft-news-service-user/src/main/java/com/soft1851/user/mapper/FansMapper.java
@@ -0,0 +1,9 @@
+package com.soft1851.user.mapper;
+
+import com.soft1851.pojo.Fans;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+@Repository
+public interface FansMapper extends Mapper<Fans> {
+}
