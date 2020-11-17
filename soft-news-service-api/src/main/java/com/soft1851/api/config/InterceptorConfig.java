37e5dfae1e7adbe1cae335a83dec0ca5808367ab
hjl <jingliqaq@gmail.com>
2020-11-17 22:17:50 +0800

    缓存用户信息，用户会话拦截器

--- soft-news-service-api/src/main/java/com/soft1851/api/config/InterceptorConfig.java
+++ soft-news-service-api/src/main/java/com/soft1851/api/config/InterceptorConfig.java
@@ -1,6 +1,7 @@
 package com.soft1851.api.config;
 
 import com.soft1851.api.interceptors.PassportInterceptor;
+import com.soft1851.api.interceptors.UserTokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -11,9 +12,16 @@ public class InterceptorConfig  implements WebMvcConfigurer {
   public PassportInterceptor passportInterceptor(){
       return new PassportInterceptor();
   }
+  @Bean
+  public UserTokenInterceptor userTokenInterceptor(){
+    return new UserTokenInterceptor();
+  }
   @Override
     public void addInterceptors(InterceptorRegistry registry){
       registry.addInterceptor(passportInterceptor())
               .addPathPatterns("/passport/smsCode");
+      registry.addInterceptor(userTokenInterceptor())
+              .addPathPatterns("/user/userBasicInfo")
+              .addPathPatterns("/user/updateUserInfo");
   }
 }
